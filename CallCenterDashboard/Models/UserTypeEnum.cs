bee9ce91f563332831e6b43acde05a741bc9c101
Jason Shave <28457536+jasonshave@users.noreply.github.com>
2022-07-25 17:46:08 -0600

    fixes #12

---
+++ CallCenterDashboard/Models/UserTypeEnum.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace CallCenterDashboard.Models;
+
+public enum UserTypeEnum
+{
+    [EnumMember(Value = "ACS User")]
+    AcsUser,
+    [EnumMember(Value = "Phone Number")]
+    PhoneNumber,
+    [EnumMember(Value = "Teams User")]
+    TeamsUser
+}

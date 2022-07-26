36898cadacd15fa1128304c1268a19389c6ea0f6
huachuandeng <73561849+huachuandeng@users.noreply.github.com>
2022-07-26 11:21:59 -0700

    add repository (#21)

---
+++ CallCenterDashboard/Repositories/InMemoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using CallCenterDashboard.Interfaces;
+using System.Collections.Concurrent;
+
+namespace CallCenterDashboard.Repositories
+{
+    public class InMemoryRepository<T> : IRepository<T>
+    {
+        private IDictionary<string, T> _data;
+
+        public InMemoryRepository()
+        {
+            _data = new ConcurrentDictionary<string, T>();
+        }
+
+        public void Add(string key, T value)
+        {
+            _data.TryAdd(key, value);
+        }
+
+        public void Remove(string key)
+        {
+            _data.Remove(key);
+        }
+
+        public T Get(string key)
+        {
+            T value = default;
+            _data.TryGetValue(key, out value);
+            return value;
+        }
+
+        public void Update(string key, T value)
+        {
+            _data[key] = value;
+        }
+
+        public IEnumerable<KeyValuePair<string, T>>GetAll()
+        {
+            return _data.Select(record => record);
+        }
+    }
+}

6cc0507f8a1f612f7684c0c28938727066de6d6b
James Deng <jamesdeng@microsoft.com>
2022-07-26 11:16:36 -0700

    add repository

---
+++ CallCenterDashboard/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace CallCenterDashboard.Interfaces
+{
+    public interface IRepository<T>
+    {
+        void Add(string key, T value);
+        void Update(string key, T value);
+        void Remove(string key);
+        T Get(string key);
+        IEnumerable<KeyValuePair<string, T>> GetAll();
+    }
+}

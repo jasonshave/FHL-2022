d792773e2d7ef6468338bd1fe6f812f18757516a
huachuandeng <73561849+huachuandeng@users.noreply.github.com>
2022-07-28 17:01:13 -0700

    James/add call detail page (#39)
    * add detail page
    
    * fix call detail page
    
    * add breadcrumb and change patch to Calls
    
    * address feedback and fix showing old data problem
    
    * fix reducer  function name
    
    * change style

---
+++ CallCenterDashboard/Features/CallDetails/CallDetailsActions.cs
@@ -0,0 +1,11 @@
+﻿using Azure.Communication.CallingServer;
+
+namespace CallCenterDashboard.Features.CallDetails;
+public record CallDetailInitializeAction(string Id);
+
+public record CallDetailSetDataAction(
+    string Id,
+    CallConnectionProperties CallConnectionProperties, 
+    IEnumerable<CallParticipant> Participants);
+
+
---
+++ CallCenterDashboard/Features/CallDetails/CallDetailsReducers.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Communication.CallingServer;
+using Fluxor;
+using System.Collections.Concurrent;
+
+namespace CallCenterDashboard.Features.CallDetails
+{
+    public static class CallDetailsReducers
+    {
+        [ReducerMethod]
+        public static CallDetailsState SetCallDetail(CallDetailsState state, CallDetailSetDataAction action)
+        {
+            return state with
+            {
+                PreviousId = action.Id,
+                CallConnectionProperties = action.CallConnectionProperties,
+                Participants = action.Participants
+            };
+        }
+    }
+
+}

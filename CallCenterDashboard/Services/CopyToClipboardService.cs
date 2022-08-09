12af75a2ac04b8ef81b94b1e76485286b5cd6248
Jason Shave <28457536+jasonshave@users.noreply.github.com>
2022-08-08 20:47:37 -0600

    fixes #63

---
+++ CallCenterDashboard/Services/CopyToClipboardService.cs
@@ -0,0 +1,19 @@
+﻿using CallingDashboard.Interfaces;
+using Microsoft.JSInterop;
+
+namespace CallingDashboard.Services;
+
+public class ClipboardService : IClipboardService
+{
+    private readonly IJSRuntime _jsInterop;
+
+    public ClipboardService(IJSRuntime jsInterop)
+    {
+        _jsInterop = jsInterop;
+    }
+
+    public async Task CopyToClipboard(string text)
+    {
+        await _jsInterop.InvokeVoidAsync("navigator.clipboard.writeText", text);
+    }
+}
